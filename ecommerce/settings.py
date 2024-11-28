dcf84ec0154914542406d567203f0f47a88c3c53
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-11-28 18:18:06 -0500

    setting static files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -115,8 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 
